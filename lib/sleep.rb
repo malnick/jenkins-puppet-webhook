aa34b9925e0af09cf96bc1189288ef99bb53f742
malnick <malnick@gmail..com>
2015-05-04 16:14:53 -0700

    sleep cycles

---
+++ lib/sleep.rb
@@ -0,0 +1,11 @@
+Module update
+  Class sleep
+    def initialize(time)
+      Log.info(##### Sleep Cycle with S3 Updates #####)
+      time.times do |n|
+        Log.info("Running on MCO in #{time - n} seconds")   
+        n = n - 1
+      end
+    end
+  end
+end
