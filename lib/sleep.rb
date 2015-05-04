86c92e87770fc87239196883e78c8973d6397f3c
malnick <malnick@gmail..com>
2015-05-04 16:22:19 -0700

    lib/sleep.rb

--- lib/sleep.rb
+++ lib/sleep.rb
@@ -1,9 +1,9 @@
 module Update
   class Sleep
     def initialize(time)
-      Log.info("##### Sleep Cycle with S3 Updates #####")
+      LOG.info("##### Sleep Cycle with S3 Updates #####")
       time.times do |n|
-        Log.info("Running on MCO in #{time - n} seconds")   
+        LOG.info("Running on MCO in #{time - n} seconds")   
         n = n - 1
       end
     end
