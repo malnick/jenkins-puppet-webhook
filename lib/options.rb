5d608adf626366c71fac4106d349827c9147ecdc
malnick <malnick@gmail..com>
2015-10-24 20:58:10 -0700

    options.rb

--- lib/options.rb
+++ lib/options.rb
@@ -4,7 +4,7 @@ module Update
     attr_accessor :config
 
     def initialize(options)
-      pv = get_puppet_version()
+#      pv = get_puppet_version()
     
       LOG.info("##### Parsing Options #####")
   
@@ -21,11 +21,11 @@ module Update
       @config[:git_repo]       = options['git_repo']        || 'git@github.com:sourceclear/puppet-control'
       @config[:git_repo_dir]   = options['git_repo_dir']    || '/etc/puppetlabs/puppet/environments/production'
       
-      if pv == '4.2.2'
-        @config[:data_file]    = '/etc/puppetlabs/code/environments/production/hieradata'
-      else
+  #    if pv == '4.2.2'
+  #      @config[:data_file]    = '/etc/puppetlabs/code/environments/production/hieradata'
+  #    else
         @config[:data_file]    = options['data_file_path']  || "#{@config[:git_repo_dir]}/hieradata/global.yaml"
-      end
+  #    end
       
       LOG.info("##### Setting configuration #####")
       @config.each do |k,v|
@@ -33,10 +33,10 @@ module Update
       end
     end
 
-    def get_puppet_version
-      io.popen('puppet --version') do |o|
-        return o.read
-      end 
-    end
+ #   def get_puppet_version
+ #     io.popen('puppet --version') do |o|
+ #       return o.read
+ #     end 
+ #   end
   end
 end
